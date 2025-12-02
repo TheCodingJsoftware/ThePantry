f7b7120ee83e1265db348744d6d02e02c033fba6
Jared Gross <jaredgrozz@gmail.com>
2025-12-01 21:27:37 -0600

    Implement code structure updates and remove redundant code blocks

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,7 @@
 import "beercss"
 import "material-dynamic-colors"
 import "@utils/theme"
+import "@components/common/footer/footer-component"
 import { DEFAULT_COLOR } from "@utils/colors"
 
 document.addEventListener("DOMContentLoaded", () => {
